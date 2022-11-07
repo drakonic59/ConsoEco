f0e9b347675a57b1c4841d6406ea6b143793bf7b
ilias <iliasdavid@live.fr>
2022-11-07 10:55:22 +0100

    Initialisation du projet SpringBoot

---
+++ src/main/java/consoeco/webservice/ConsoEcoServiceApplication.java
@@ -0,0 +1,13 @@
+package consoeco.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsoEcoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsoEcoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/consoeco/webservice/ConsoEcoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package consoeco.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsoEcoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
